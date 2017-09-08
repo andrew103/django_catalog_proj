7998520390307603c2d46e7a0676342aed40073b
Andrew Euredjian <andrew_103@rocketmail.com>
2017-09-08 09:51:26 -0400

    Add urls to connect apps

--- catalog/catalog/urls.py
+++ catalog/catalog/urls.py
@@ -20,4 +20,8 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.HomePage.as_view(), name = 'home'),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls')), #allows the accounts/urls file to use the built in LogIn/LogOut views
+    url(r'^categories/', include('categories.urls', namespace='categories')),
+    url(r'^categories/items/', include('items.urls', namespace='items')),
 ]
