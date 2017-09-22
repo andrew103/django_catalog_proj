9b932eac41c0249523b1964fdbeb5035eb62031c
Andrew Euredjian <andrew_103@rocketmail.com>
2017-09-22 13:16:56 -0400

    Revert to only having one type of user with all capabilities rather than a user that can buy and a user that can buy/sell

--- catalog/accounts/forms.py
+++ catalog/accounts/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-class CustomerCreateForm(UserCreationForm):
+class UserCreateForm(UserCreationForm):
 
     class Meta:
         fields = ('username', 'email', 'password1', 'password2')
@@ -11,16 +11,3 @@ class CustomerCreateForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].label = 'Display Name'
         self.fields['email'].label = 'Email Address'
-
-
-class SellerCreateForm(UserCreationForm):
-
-    class Meta:
-        fields = ('username', 'email', 'company', 'password1', 'password2')
-        model = get_user_model()
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].label = 'Display Name'
-        self.fields['email'].label = 'Email Address'
-        self.fields['company'] = 'Company'
--- catalog/accounts/views.py
+++ catalog/accounts/views.py
@@ -5,12 +5,7 @@ from django.views.generic import CreateView
 from . import forms
 # Create your views here.
 
-class SignUpCustomer(CreateView):
-    form_class = forms.CustomerCreateForm
+class SignUpUser(CreateView):
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
-    template_name = 'accounts/signup_cust.html'
-
-class SignUpSeller(CreateView):
-    form_class = forms.SellerCreateForm
-    success_url = reverse_lazy('login')
-    template_name = 'accounts/signup_sell.html'
+    template_name = 'accounts/signup.html'
