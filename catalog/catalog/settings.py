f3f5256d87dd3f3b0f04d5d7282ae3bc14fcc58b
Andrew Euredjian <andrew_103@rocketmail.com>
2017-09-26 09:33:35 -0400

    Fix login/logout "no reverse match" error

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
