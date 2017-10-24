6efd24bbdb431a78169b219658bc7a120ac5ed23
Andrew Euredjian <andrew_103@rocketmail.com>
2017-10-24 10:34:33 -0400

    Create skeleton for categories model

--- catalog/categories/models.py
+++ catalog/categories/models.py
@@ -1,3 +1,15 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.text import slugify
+
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django import template
+register = template.Library()
 
 # Create your models here.
+
+class Categories(models.Model):
+    pass
